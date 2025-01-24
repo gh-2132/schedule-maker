f8d51f7dbc9288fc4bc95d8a2e26b741b4658baf
gh-2132 <163818355+gh-2132@users.noreply.github.com>
2025-01-23 17:30:15 -0800

    Added New Widgets

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
-class MainWindow(QApplication):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(700, 500)
+        self.setWindowTitle("Calendar")
+        self.setGeometry(0, 0, 1000, 700)
         self.initUI()
+
+    def initUI(self):
+        # Create the calendar
+        calendar = QCalendarWidget(self)
+        calendar.setGeometry(0, 0, 1000, 600)
+
+        # Create the text at the bottom
+        dateLabel = QLabel(self)
+        dateLabel.setGeometry(0, 650, 1000, 50)
+        dateLabel.setText(str(QDate.currentDate()))
+        dateLabel.setFont(QFont('Arial', 20))
+
+    def getCurrentDate(self):
+        pass
+        # Display the date
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
